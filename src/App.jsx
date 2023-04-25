9c86549a9702a6f68d9bb769472746d6d98e1780
gnrflcthm <70900049+gnrflcthm@users.noreply.github.com>
2023-04-25 09:48:27 +0800

    act-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,106 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import Product from "./components/Product";
+import React, { useState, useEffect, useMemo } from "react";
+import "./app.css";
 
 function App() {
+  const [time, setTime] = useState(new Date());
+  const [timeIn, setTimeIn] = useState();
+  const [timeOut, setTimeOut] = useState();
+  const [timeDiff, setTimeDiff] = useState();
+  const formattedTime = useMemo(() => {
+    let hours = (time.getHours() % 12).toString();
+    const minutes = time.getMinutes().toString();
+    const half = time.getHours() > 11 && time.getHours() < 24 ? "PM" : "AM";
+    hours = hours === 0 ? hours + 1 : hours;
+    return { hours, minutes: minutes.padStart(2, "0"), half };
+  }, [time]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const onTimeIn = () => {
+    setTimeIn(time);
+  };
+
+  const onTimeOut = () => {
+    if (!timeIn) return;
+
+    const lastTime = time;
+    setTimeOut(lastTime);
+    const diff = lastTime.getTime() - timeIn.getTime();
+    setTimeDiff(diff);
+    console.log(diff);
+  };
+
+  const onClear = () => {
+    setTimeIn(null);
+    setTimeOut(null);
+    setTimeDiff(null);
+  };
+
+  function formatDiff() {
+    if (!timeDiff) {
+      return;
+    }
+
+    let tempMillis = timeDiff;
+    let hours = tempMillis >= 360000 ? Math.floor(tempMillis / 360000) : 0;
+    tempMillis -= hours * 3_600_000;
+    let minutes = tempMillis >= 60000 ? Math.floor(tempMillis / 60000) : 0;
+    tempMillis -= minutes * 60000;
+    let seconds = tempMillis >= 1000 ? Math.floor(tempMillis / 1000) : 0;
+    tempMillis -= Math.floor(seconds * 1000);
+    return `${padZeros(hours)}:${padZeros(minutes)}:${padZeros(
+      seconds
+    )}.${tempMillis}`;
+  }
+
+  function padZeros(val) {
+    return String(val).padStart(2, "0");
+  }
+
   return (
-    <div className="container p-4">
-      <h1 className="w-100 text-center my-4">Products</h1>
-      <div className="row g-2 row-cols-4">
-        <Product
-          description={"Just Do It."}
-          name={"Adios"}
-          price={499.99}
-          src={
-            "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg?w=1380&t=st=1676974470~exp=1676975070~hmac=82714c9b022657fecc1f8bb18243ea8fafc37d20990348cd1a7a9140c7bea048"
-          }
-        />
-        <Product
-          description={"Just Do It."}
-          name={"Wans"}
-          price={500.01}
-          src={
-            "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg?w=1380&t=st=1676974470~exp=1676975070~hmac=82714c9b022657fecc1f8bb18243ea8fafc37d20990348cd1a7a9140c7bea048"
-          }
-        />
-        <Product
-          description={"Just Do It."}
-          name={"Michaels"}
-          price={999_999.95}
-          src={
-            "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg?w=1380&t=st=1676974470~exp=1676975070~hmac=82714c9b022657fecc1f8bb18243ea8fafc37d20990348cd1a7a9140c7bea048"
-          }
-        />
-        <Product
-          description={"Just Do It."}
-          name={"Timberlake"}
-          price={2211.99}
-          src={
-            "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg?w=1380&t=st=1676974470~exp=1676975070~hmac=82714c9b022657fecc1f8bb18243ea8fafc37d20990348cd1a7a9140c7bea048"
-          }
-        />
-        <Product
-          description={"Just Do It."}
-          name={"James 12"}
-          price={4770.50}
-          src={
-            "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg?w=1380&t=st=1676974470~exp=1676975070~hmac=82714c9b022657fecc1f8bb18243ea8fafc37d20990348cd1a7a9140c7bea048"
-          }
-        />
-        <Product
-          description={"Just Do It."}
-          name={"Hush Dogs"}
-          price={50.99}
-          src={
-            "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg?w=1380&t=st=1676974470~exp=1676975070~hmac=82714c9b022657fecc1f8bb18243ea8fafc37d20990348cd1a7a9140c7bea048"
-          }
-        />
-        <Product
-          description={"Just Do It."}
-          name={"Yeezy Soft"}
-          price={1099}
-          src={
-            "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg?w=1380&t=st=1676974470~exp=1676975070~hmac=82714c9b022657fecc1f8bb18243ea8fafc37d20990348cd1a7a9140c7bea048"
-          }
-        />
-        <Product
-          description={"Just Do It."}
-          name={"Sketchy"}
-          price={407.99}
-          src={
-            "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg?w=1380&t=st=1676974470~exp=1676975070~hmac=82714c9b022657fecc1f8bb18243ea8fafc37d20990348cd1a7a9140c7bea048"
-          }
-        />
+    <div className="app">
+      <div className="time">
+        <div className="today">
+          <h1>
+            {formattedTime.hours}
+            <span className="sep">:</span>
+            {formattedTime.minutes}
+            <span>{formattedTime.half}</span>
+          </h1>
+          <h2>{time.toDateString().split(" ").splice(1, 3).join(" ")}</h2>
+        </div>
+        <div className="timeContainer">
+          <div className="timeDisplay">
+            <h4>Time In</h4>
+            <p>{timeIn?.toLocaleTimeString() || ""}</p>
+          </div>
+          <div className="timeDisplay">
+            <h4>Time Out</h4>
+            <p>{timeOut?.toLocaleTimeString() || ""}</p>
+          </div>
+          <div className="timeDisplay">
+            <h4>Difference</h4>
+            <p>{formatDiff()}</p>
+          </div>
+        </div>
+      </div>
+      <div className="buttonContainer">
+        <button onClick={() => onTimeIn()}>
+          <span>Time In</span>
+        </button>
+        <button onClick={() => onTimeOut()}>
+          <span>Time Out</span>
+        </button>
+        <button onClick={() => onClear()}>
+          <span>Clear</span>
+        </button>
       </div>
     </div>
   );
